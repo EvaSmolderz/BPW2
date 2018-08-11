a60790c5c6d0767f69ea3d72aa1c0412aed188f8
EvaSmolderz <36568809+EvaSmolderz@users.noreply.github.com>
2018-08-11 19:58:01 +0200

    Editu
    editu

---
+++ SmoldersEvaBPW2/Assets/MoveScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BPWAsset : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector2 MoveVelocity;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        MoveVelocity = moveInput.normalized * speed;
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + MoveVelocity * Time.fixedDeltaTime);
+    }
+}
