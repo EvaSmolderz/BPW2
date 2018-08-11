30435629742ba3c9f3c8456b66d71541a70108f5
EvaSmolderz <36568809+EvaSmolderz@users.noreply.github.com>
2018-08-11 20:46:05 +0200

    Collision and restart game after touch
    can't go through walls and when the enemy touches you the game restarts

--- SmoldersEvaBPW2/Assets/EnemyScript.cs
+++ SmoldersEvaBPW2/Assets/EnemyScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemyScript : MonoBehaviour {
 
@@ -8,19 +9,28 @@ public class EnemyScript : MonoBehaviour {
 
     private Transform target;
 
-	// Use this for initialization
-	void Start () {
+    private Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Start () {
 
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-
-	}
+        rb2d = GetComponent<Rigidbody2D>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
         if(Vector2.Distance(transform.position, target.position) < 30) {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-
+            rb2d.velocity = (target.position - transform.position).normalized * speed;
         }
 	}
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
 }
--- SmoldersEvaBPW2/Assets/MovingScript.cs
+++ SmoldersEvaBPW2/Assets/MovingScript.cs
@@ -4,25 +4,30 @@ using UnityEngine;
 
 public class MovingScript : MonoBehaviour
 {
-    public float speed;
-    private Rigidbody2D rb;
-    private Vector2 MoveVelocity;
+    public float speed;             //Floating point variable to store the player's movement speed.
 
-    private void Start()
+    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+
+    // Use this for initialization
+    void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
     {
-        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        MoveVelocity = moveInput.normalized * speed;
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis("Horizontal");
 
-    }
+        //Store the current vertical input in the float moveVertical.
+        float moveVertical = Input.GetAxis("Vertical");
 
-    private void FixedUpdate()
-    {
-        rb.MovePosition(rb.position + MoveVelocity * Time.fixedDeltaTime);
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        rb2d.velocity = movement * speed;
     }
 }
